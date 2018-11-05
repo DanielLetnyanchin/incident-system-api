3b90d295b54a589deaebb1ae9cf914013c6ca1d3
Daniil Letnianchyn <Daniil_Letnianchyn@epam.com>
2018-11-05 18:45:04 +0200

    Architecture correction

---
+++ IncidentSystem.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IncidentSystem.Business
+{
+    public class Class1
+    {
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -21,6 +21,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Moove to extand method all config
             services.AddDbContext<DatabaseContext>(
                 options => options
                 .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), o => o.MigrationsAssembly("WebAPI")));
