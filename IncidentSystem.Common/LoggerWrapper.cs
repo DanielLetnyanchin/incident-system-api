b373ed0c6b47b15b518bf678887585c2116379f2
Illia Dekhtiarenko <IDekhtiarenko@slb.com>
2018-11-15 16:25:29 +0200

    Added CorrelationId

--- IncidentSystem.Common/LoggerWrapper.cs
+++ IncidentSystem.Common/LoggerWrapper.cs
@@ -17,28 +17,28 @@ namespace IncidentSystem.Common
 
         public void Debug(params string[] list)
         {
-            var logRecord = string.Format("Message:{0} Stack Trace:{1}", list[0],list[1]);
+            var logRecord = string.Format("RequestId: {0};  Message: {1};  Stack Trace:{2}", list[0], list[1], list[2]);
 
             logger.Debug(logRecord);
         }
 
         public void Error(params string[] list)
         {
-            var logRecord = string.Format("Message:{0} Stack Trace:{1}", list[0], list[1]);
+            var logRecord = string.Format("RequestId: {0};  Message: {1};  Stack Trace:{2}", list[0], list[1], list[2]);
 
             logger.Error(logRecord);
         }
 
         public void Info(params string[] list)
         {
-            var logRecord = string.Format("Message:{0} Stack Trace:{1}", list[0], list[1]);
+            var logRecord = string.Format("RequestId: {0};  Message: {1};  Stack Trace:{2}", list[0], list[1], list[2]);
 
             logger.Info(logRecord);
         }
 
         public void Warn(params string[] list)
         {
-            var logRecord = string.Format("Message:{0} Stack Trace:{1}", list[0], list[1]);
+            var logRecord = string.Format("RequestId: {0};  Message: {1};  Stack Trace:{2}", list[0], list[1], list[2]);
 
             logger.Warn(logRecord);
         }
